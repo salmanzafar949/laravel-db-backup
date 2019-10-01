afa1baecadfcba5b9ca85c5a042cc6d3716e9da8
Salman Zafar <salman.zafar@innowi.com>
2019-10-01 18:49:30 +0500

    service provider created

---
+++ src/DbBackupServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Salman\DbBackup;
+
+use Illuminate\Support\ServiceProvider;
+
+class DbBackupServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
